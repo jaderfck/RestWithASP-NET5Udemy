1cd8454981752ca93878d06af371a0a42e788e06
Jader Fuck <jaderfck@gmail.com>
2020-12-08 16:30:12 -0300

    commit de hj

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Business/ILoginBusiness.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Business
+{
+    interface ILoginBusiness
+    {
+        TokenVO ValidateCredentiasl(UserVO user);
+    }
+}
